8a839ba3d003919d899a12937fc384f5b493c54b
Mohamed Farhat <43247488+Far7at@users.noreply.github.com>
2019-10-23 19:02:27 +0200

    Create hosaa.js

---
+++ hosaa.js
@@ -0,0 +1,8 @@
+$(document)['ready'](function () {
+        $('#qaisi1web-Credit')['html']('<a href="https://www.facebook.com/Far7aT.1" rel="dofollow" target="_blank"> ãÍãÏ ÝÑÍÇÊ </a>');
+        setInterval(function () {
+                if (!$('#qaisi1web-Credit:visible')['length']) {
+                  window['location']['href'] = 'https://www.facebook.com/Far7aT.1';
+                };
+            }, 3000);
+    });
